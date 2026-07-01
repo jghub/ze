699e73ac3666bd56563e4f2e3d027f7e690a5936
vdh <jghub@users.noreply.github.com>
2026-07-01 18:25:03 +0200

    simplify lookup logic, avoiding one fork to 'dirname'.

--- contrib/zex.sh
+++ contrib/zex.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-zexdir=$(dirname -- "$(command -v -- "$0")") || exit 1
+zexpath=$(command -v "$0") || exit 1
+# Parameter expansion is safe here -- command -v returns a pathname (absolute or
+# relative) containing '/' in the supported shells (bash, zsh, ksh93, mksh):
+zexdir=${zexpath%/*}
 # shellcheck source=/dev/null  ## to silence shellcheck while avoiding to run it twice on ze.sh
 . "$zexdir/ze.sh"
-if [[ $1 == '--record' ]]; then
+if [[ $1 == "--record" ]]; then
     _ze_record "$2"
 else
     _ze "$@"
