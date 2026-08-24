15413201901061ac259137fbcef2e0c114609be7
vdh <jghub@users.noreply.github.com>
2026-08-24 21:12:22 +0200

    replace 'command name' by 'builtin name' since the former does not work in vanilla zsh (user would need to do 'set -o POSIX_BUILTINS' to restore sane behaviour). thanks to user elseawhy for reporting this issue which is especially annoying since 'command cd ..' just fails silently in vanilla zsh.

--- ze.sh
+++ ze.sh
@@ -87,12 +87,12 @@ function _ze_dirs {
 }
 
 function _ze_cd {
-    if command cd "$@"; then
+    if builtin cd "$@"; then
         # ksh93 may emit job-control notifications for the backgrounded helper despite wrapping it in
         # a subshell (this happens not in all terminal emulators, but in most). this makes
         # redirection of stderr to /dev/null necessary.
         if [[ ${_ZE_RESOLVE_SYMLINKS:-} ]]; then
-            (_ze_record "$(command pwd -P 2>/dev/null)" &) 2>/dev/null
+            (_ze_record "$(builtin pwd -P 2>/dev/null)" &) 2>/dev/null
         else
             (_ze_record "$PWD" &) 2>/dev/null
         fi
