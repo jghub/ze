f697ab06dc7f1aac39c36aba9758427a4e898463
vdh <jghub@users.noreply.github.com>
2026-09-04 13:55:14 +0200

    mostly manpage adjustments.

--- ze.sh
+++ ze.sh
@@ -252,7 +252,7 @@ function _ze {
                 o) open=1; mode=files;;
                 r) typ="visits";;
                 t) typ="recent";;
-                V) printf '%s\n' "ze v3.3.2+"; return;;
+                V) typeset ze_version="ze v3.3.2+"; printf '%s\n' "$ze_version"; return;;
                 *) ;;   # silently ignore unrecognized options
             esac; opt=${opt:1}; done;;
          *) fnd+=${fnd:+ }$1; fdargs+=("$1");;
